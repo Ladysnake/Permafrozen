10c8b508afc74a80c1e45886e5af1b8e3f9404ab
Alexander3847575 <51353507+Alexander3847575@users.noreply.github.com>
2021-03-28 21:33:08 -0700

    Add cobalt armor

--- src/main/java/permafrozen/registry/ItemRegistry.java
+++ src/main/java/permafrozen/registry/ItemRegistry.java
@@ -1,5 +1,6 @@
 package permafrozen.registry;
 
+import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.Item;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -8,6 +9,7 @@ import permafrozen.item.*;
 import permafrozen.item.wulfrum.*;
 import permafrozen.item.cobalt.*;
 import permafrozen.item.chillorite.*;
+import permafrozen.util.PermafrozenArmorMaterial;
 
 import java.lang.reflect.Field;
 
@@ -32,6 +34,10 @@ public class ItemRegistry {
     public static final Item COBALT_AXE = new CobaltAxe().setRegistryName("cobalt_axe");
     public static final Item COBALT_SHOVEL = new CobaltShovel().setRegistryName("cobalt_shovel");
     public static final Item COBALT_HOE = new CobaltShovel().setRegistryName("cobalt_hoe");
+    public static final Item COBALT_HELMET = new CobaltArmor(PermafrozenArmorMaterial.COBALT, EquipmentSlotType.HEAD).setArmorTexture("cobalt_layer_1").setRegistryName("cobalt_helmet");
+    public static final Item COBALT_CHESTPLATE = new CobaltArmor(PermafrozenArmorMaterial.COBALT, EquipmentSlotType.CHEST).setArmorTexture("cobalt_layer_1").setRegistryName("cobalt_chestplate");
+    public static final Item COBALT_LEGS = new CobaltArmor(PermafrozenArmorMaterial.COBALT, EquipmentSlotType.LEGS).setArmorTexture("cobalt_layer_2").setRegistryName("cobalt_leggings");
+    public static final Item COBALT_BOOTS = new CobaltArmor(PermafrozenArmorMaterial.COBALT, EquipmentSlotType.FEET).setArmorTexture("cobalt_layer_1").setRegistryName("cobalt_boots");
 
     @SubscribeEvent
     public static void onItemsRegistration(final RegistryEvent.Register<Item> e) {
