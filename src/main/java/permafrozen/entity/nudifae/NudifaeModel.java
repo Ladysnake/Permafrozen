2b48d6146edd485a5c446fe40060a05e3e40457a
Alexander3847575 <51353507+Alexander3847575@users.noreply.github.com>
2021-05-09 21:42:52 -0700

    melanistic nudifae

--- src/main/java/permafrozen/entity/nudifae/NudifaeModel.java
+++ src/main/java/permafrozen/entity/nudifae/NudifaeModel.java
@@ -3,7 +3,10 @@ package permafrozen.entity.nudifae;
 import net.minecraft.util.ResourceLocation;
 import permafrozen.Permafrozen;
 import permafrozen.entity.Nudifae;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.processor.IBone;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
+import software.bernie.geckolib3.model.provider.data.EntityModelData;
 
 public class NudifaeModel extends AnimatedGeoModel<Nudifae> {
 
@@ -26,6 +29,17 @@ public class NudifaeModel extends AnimatedGeoModel<Nudifae> {
 
 		return new ResourceLocation(Permafrozen.MOD_ID, "animations/nudifae.animation.json");
 
+	}
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@Override
+	public void setLivingAnimations(Nudifae entity, Integer uniqueID, AnimationEvent customPredicate) {
+		super.setLivingAnimations(entity, uniqueID, customPredicate);
+		IBone head = this.getAnimationProcessor().getBone("head");
+
+		EntityModelData extraData = (EntityModelData) customPredicate.getExtraDataOfType(EntityModelData.class).get(0);
+		head.setRotationX((extraData.headPitch + 30) * ((float) Math.PI / 360F));
+		head.setRotationY((extraData.netHeadYaw) * ((float) Math.PI / 500F));
 	}
 
 }
