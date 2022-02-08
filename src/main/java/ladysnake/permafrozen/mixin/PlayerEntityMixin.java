ccf670b92e132aa54b20bcab5af036ef97c0138c
Arathain <arathainfarqoe@gmail.com>
2022-02-08 09:53:52 +0100

    //just a few tidbits

---
+++ src/main/java/ladysnake/permafrozen/mixin/PlayerEntityMixin.java
@@ -0,0 +1,22 @@
+package ladysnake.permafrozen.mixin;
+
+import ladysnake.permafrozen.registry.PermafrozenComponents;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PlayerEntity.class)
+public abstract class PlayerEntityMixin extends LivingEntity{
+    protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
+        super(entityType, world);
+    }
+    @Inject(at = @At("HEAD"), method = "tick")
+    private void permafrozenPlayerTick(CallbackInfo ci) {
+        //PermafrozenComponents.PLAYER
+    }
+}
